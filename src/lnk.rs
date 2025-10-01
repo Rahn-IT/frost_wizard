6440e6ff5fb5fe7415110a1504c62a061c500e6b
L. Wlcek <luca@it-rahn.de>
2025-10-02 00:20:20 +0200

    fixed flags... again...

--- src/lnk.rs
+++ src/lnk.rs
@@ -66,6 +66,7 @@ impl Lnk {
         }
 
         let link_flags = read_flags(data)?;
+        println!("link_flags: {link_flags:032b}");
         let link_flags = LinkFlags::from_bits(link_flags)
             .ok_or_else(|| LnkParseError::InvalidLinkFlags(link_flags))?;
 
@@ -182,7 +183,7 @@ fn read_i32(data: &mut impl Read) -> io::Result<i32> {
 
 #[must_use]
 fn read_flags(data: &mut impl Read) -> io::Result<u32> {
-    data.read_u32::<BE>()
+    data.read_u32::<LE>()
 }
 
 #[must_use]
@@ -340,118 +341,118 @@ bitflags! {
         /// The shell link is saved with an item ID list (IDList). If this bit is set, a
         /// LinkTargetIDList structure (section 2.2) MUST follow the ShellLinkHeader.
         /// If this bit is not set, this structure MUST NOT be present.
-        const HAS_LINK_TARGET_ID_LIST           = 0b1000_0000_0000_0000_0000_0000_0000_0000;
+        const HAS_LINK_TARGET_ID_LIST           = 0b0000_0000_0000_0000_0000_0000_0000_0001;
 
         /// The shell link is saved with link information. If this bit is set, a LinkInfo
         /// structure (section 2.3) MUST be present. If this bit is not set, this structure
         /// MUST NOT be present.
-        const HAS_LINK_INFO                     = 0b0100_0000_0000_0000_0000_0000_0000_0000;
+        const HAS_LINK_INFO                     = 0b0000_0000_0000_0000_0000_0000_0000_0010;
 
         ///The shell link is saved with a name string. If this bit is set, a
         ///NAME_STRING StringData structure (section 2.4) MUST be present. If
         ///this bit is not set, this structure MUST NOT be present.
-        const HAS_NAME                          = 0b0010_0000_0000_0000_0000_0000_0000_0000;
+        const HAS_NAME                          = 0b0000_0000_0000_0000_0000_0000_0000_0100;
 
         /// The shell link is saved with a relative path string. If this bit is set, a
         /// RELATIVE_PATH StringData structure (section 2.4) MUST be present. If
         /// this bit is not set, this structure MUST NOT be present.
-        const HAS_RELATIVE_PATH                 = 0b0001_0000_0000_0000_0000_0000_0000_0000;
+        const HAS_RELATIVE_PATH                 = 0b0000_0000_0000_0000_0000_0000_0000_1000;
 
         /// The shell link is saved with a working directory string. If this bit is set, a
         /// WORKING_DIR StringData structure (section 2.4) MUST be present. If
         /// this bit is not set, this structure MUST NOT be present.
-        const HAS_WORKING_DIR                   = 0b0000_1000_0000_0000_0000_0000_0000_0000;
+        const HAS_WORKING_DIR                   = 0b0000_0000_0000_0000_0000_0000_0001_0000;
 
         /// The shell link is saved with command line arguments. If this bit is set, a
         /// COMMAND_LINE_ARGUMENTS StringData structure (section 2.4) MUST
         /// be present. If this bit is not set, this structure MUST NOT be present.
-        const HAS_ARGUMENTS                     = 0b0000_0100_0000_0000_0000_0000_0000_0000;
+        const HAS_ARGUMENTS                     = 0b0000_0000_0000_0000_0000_0000_0010_0000;
 
         /// The shell link is saved with an icon location string. If this bit is set, an
         /// ICON_LOCATION StringData structure (section 2.4) MUST be present. If
         /// this bit is not set, this structure MUST NOT be present.
-        const HAS_ICON_LOCATION                 = 0b0000_0010_0000_0000_0000_0000_0000_0000;
+        const HAS_ICON_LOCATION                 = 0b0000_0000_0000_0000_0000_0000_0100_0000;
 
         /// The shell link contains Unicode encoded strings. This bit SHOULD be set. If
         /// this bit is set, the StringData section contains Unicode-encoded strings;
         /// otherwise, it contains strings that are encoded using the system default
         /// code page.
-        const IS_UNICODE                        = 0b0000_0001_0000_0000_0000_0000_0000_0000;
+        const IS_UNICODE                        = 0b0000_0000_0000_0000_0000_0000_1000_0000;
 
         /// The LinkInfo structure (section 2.3) is ignored.
-        const FORCE_NO_LINK_INFO                = 0b0000_0000_1000_0000_0000_0000_0000_0000;
+        const FORCE_NO_LINK_INFO                = 0b0000_0000_0000_0000_0000_0001_0000_0000;
 
         /// The shell link is saved with an
         /// EnvironmentVariableDataBlock (section 2.5.4).
-        const HAS_EXP_STRING                    = 0b0000_0000_0100_0000_0000_0000_0000_0000;
+        const HAS_EXP_STRING                    = 0b0000_0000_0000_0000_0000_0010_0000_0000;
 
         /// The target is run in a separate virtual machine when launching a link
         /// target that is a 16-bit application.
-        const RUN_IN_SEPARATE_PROCESS           = 0b0000_0000_0010_0000_0000_0000_0000_0000;
+        const RUN_IN_SEPARATE_PROCESS           = 0b0000_0000_0000_0000_0000_0100_0000_0000;
 
         /// A bit that is undefined and MUST be ignored.
-        const UNUSED_1                          = 0b0000_0000_0001_0000_0000_0000_0000_0000;
+        const UNUSED_1                          = 0b0000_0000_0000_0000_0000_1000_0000_0000;
 
         /// The shell link is saved with a DarwinDataBlock (section 2.5.3).
-        const HAS_DARWIN_ID                     = 0b0000_0000_0000_1000_0000_0000_0000_0000;
+        const HAS_DARWIN_ID                     = 0b0000_0000_0000_0000_0001_0000_0000_0000;
 
         /// The application is run as a different user when the target of the shell link is
         /// activated.
-        const RUN_AS_USER                       = 0b0000_0000_0000_0100_0000_0000_0000_0000;
+        const RUN_AS_USER                       = 0b0000_0000_0000_0000_0010_0000_0000_0000;
 
         /// The shell link is saved with an IconEnvironmentDataBlock (section 2.5.5).
-        const HAS_EXP_ICON                      = 0b0000_0000_0000_0010_0000_0000_0000_0000;
+        const HAS_EXP_ICON                      = 0b0000_0000_0000_0000_0100_0000_0000_0000;
 
         /// The file system location is represented in the shell namespace when the
         /// path to an item is parsed into an IDList.
-        const NO_PID_I_ALIAS                    = 0b0000_0000_0000_0001_0000_0000_0000_0000;
+        const NO_PID_I_ALIAS                    = 0b0000_0000_0000_0000_1000_0000_0000_0000;
 
         /// A bit that is undefined and MUST be ignored.
-        const UNUSED_2                          = 0b0000_0000_0000_0000_1000_0000_0000_0000;
+        const UNUSED_2                          = 0b0000_0000_0000_0001_0000_0000_0000_0000;
 
         /// The shell link is saved with a ShimDataBlock (section 2.5.8).
-        const RUN_WITH_SHIM_LAYER               = 0b0000_0000_0000_0000_0100_0000_0000_0000;
+        const RUN_WITH_SHIM_LAYER               = 0b0000_0000_0000_0010_0000_0000_0000_0000;
 
         /// The TrackerDataBlock (section 2.5.10) is ignored.
-        const FORCE_NO_LINK_TRACK               = 0b0000_0000_0000_0000_0010_0000_0000_0000;
+        const FORCE_NO_LINK_TRACK               = 0b0000_0000_0000_0100_0000_0000_0000_0000;
 
         /// The shell link attempts to collect target properties and store them in the
         /// PropertyStoreDataBlock (section 2.5.7) when the link target is set.
-        const ENABLE_TARGET_METADATA            = 0b0000_0000_0000_0000_0001_0000_0000_0000;
+        const ENABLE_TARGET_METADATA            = 0b0000_0000_0000_1000_0000_0000_0000_0000;
 
         /// The EnvironmentVariableDataBlock is ignored.
-        const DISABLE_LINK_PATH_TRACKING        = 0b0000_0000_0000_0000_0000_1000_0000_0000;
+        const DISABLE_LINK_PATH_TRACKING        = 0b0000_0000_0001_0000_0000_0000_0000_0000;
 
         /// The SpecialFolderDataBlock (section 2.5.9) and the
         /// KnownFolderDataBlock (section 2.5.6) are ignored when loading the shell
         /// link. If this bit is set, these extra data blocks SHOULD NOT be saved when
         /// saving the shell link.
-        const DISABLE_KNOWN_FOLDER_TRACKING     = 0b0000_0000_0000_0000_0000_0100_0000_0000;
+        const DISABLE_KNOWN_FOLDER_TRACKING     = 0b0000_0000_0010_0000_0000_0000_0000_0000;
 
         /// If the link has a KnownFolderDataBlock (section 2.5.6), the unaliased form
         /// of the known folder IDList SHOULD be used when translating the target
         /// IDList at the time that the link is loaded.
-        const DISABLE_KNOWN_FOLDER_ALIAS        = 0b0000_0000_0000_0000_0000_0010_0000_0000;
+        const DISABLE_KNOWN_FOLDER_ALIAS        = 0b0000_0000_0100_0000_0000_0000_0000_0000;
 
         /// Creating a link that references another link is enabled. Otherwise,
         /// specifying a link as the target IDList SHOULD NOT be allowed.
-        const ALLOW_LINK_TO_LINK                = 0b0000_0000_0000_0000_0000_0001_0000_0000;
+        const ALLOW_LINK_TO_LINK                = 0b0000_0000_1000_0000_0000_0000_0000_0000;
 
         /// When saving a link for which the target IDList is under a known folder,
         /// either the unaliased form of that known folder or the target IDList SHOULD
         /// be used.
-        const UNALIAS_ON_SAVE                   = 0b0000_0000_0000_0000_0000_0000_1000_0000;
+        const UNALIAS_ON_SAVE                   = 0b0000_0001_0000_0000_0000_0000_0000_0000;
 
         /// The target IDList SHOULD NOT be stored; instead, the path specified in the
         /// EnvironmentVariableDataBlock (section 2.5.4) SHOULD be used to refer to
         /// the target.
-        const PREFER_ENVIRONMENT_PATH           = 0b0000_0000_0000_0000_0000_0000_0100_0000;
+        const PREFER_ENVIRONMENT_PATH           = 0b0000_0010_0000_0000_0000_0000_0000_0000;
 
         /// When the target is a UNC name that refers to a location on a local
         /// machine, the local path IDList in the
         /// PropertyStoreDataBlock (section 2.5.7) SHOULD be stored, so it can be
         /// used when the link is loaded on the local machine.
-        const KEEP_LOCAL_ID_LIST_FOR_UNC_TARGET = 0b0000_0000_0000_0000_0000_0000_0010_0000;
+        const KEEP_LOCAL_ID_LIST_FOR_UNC_TARGET = 0b0000_0100_0000_0000_0000_0000_0000_0000;
     }
 }
 
@@ -463,49 +464,49 @@ bitflags! {
     #[derive(Debug, Clone)]
     struct FileAttributeFlags: u32 {
         /// The file or directory is read-only. For a file, if this bit is set, applications can read the file but cannot write to it or delete it. For a directory, if this bit is set, applications cannot delete the directory.
-        const FILE_ATTRIBUTE_READONLY               = 0b1000_0000_0000_0000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_READONLY               = 0b0000_0000_0000_0000_0000_0000_0000_0001;
 
         /// The file or directory is hidden. If this bit is set, the file or folder is not included in an ordinary directory listing.
-        const FILE_ATTRIBUTE_HIDDEN                 = 0b0100_0000_0000_0000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_HIDDEN                 = 0b0000_0000_0000_0000_0000_0000_0000_0010;
 
         /// The file or directory is part of the operating system or is used exclusively by the operating system.
-        const FILE_ATTRIBUTE_SYSTEM                 = 0b0010_0000_0000_0000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_SYSTEM                 = 0b0000_0000_0000_0000_0000_0000_0000_0100;
 
         /// A bit that MUST be zero.
-        const RESERVED_1                            = 0b0001_0000_0000_0000_0000_0000_0000_0000;
+        const RESERVED_1                            = 0b0000_0000_0000_0000_0000_0000_0000_1000;
 
         /// The link target is a directory instead of a file.
-        const FILE_ATTRIBUTE_DIRECTORY              = 0b0000_1000_0000_0000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_DIRECTORY              = 0b0000_0000_0000_0000_0000_0000_0001_0000;
 
         /// The file or directory is an archive file. Applications use this flag to mark files for backup or removal.
-        const FILE_ATTRIBUTE_ARCHIVE                = 0b0000_0100_0000_0000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_ARCHIVE                = 0b0000_0000_0000_0000_0000_0000_0010_0000;
 
         /// A bit that MUST be zero.
-        const RESERVED_2                            = 0b0000_0010_0000_0000_0000_0000_0000_0000;
+        const RESERVED_2                            = 0b0000_0000_0000_0000_0000_0000_0100_0000;
 
         /// The file or directory has no other flags set. If this bit is 1, all other bits in this structure MUST be clear.
-        const FILE_ATTRIBUTE_NORMAL                 = 0b0000_0001_0000_0000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_NORMAL                 = 0b0000_0000_0000_0000_0000_0000_1000_0000;
 
         /// The file is being used for temporary storage.
-        const FILE_ATTRIBUTE_TEMPORARY              = 0b0000_0000_1000_0000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_TEMPORARY              = 0b0000_0000_0000_0000_0000_0001_0000_0000;
 
         /// The file is a sparse file.
-        const FILE_ATTRIBUTE_SPARCE_FILE            = 0b0000_0000_0100_0000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_SPARCE_FILE            = 0b0000_0000_0000_0000_0000_0010_0000_0000;
 
         /// The file or directory has an associated reparse point.
-        const FILE_ATTRIBUTE_REPARSE_POINT          = 0b0000_0000_0010_0000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_REPARSE_POINT          = 0b0000_0000_0000_0000_0000_0100_0000_0000;
 
         /// The file or directory is compressed. For a file, this means that all data in the file is compressed. For a directory, this means that compression is the default for newly created files and subdirectories.
-        const FILE_ATTRIBUTE_COMPRESSED             = 0b0000_0000_0001_0000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_COMPRESSED             = 0b0000_0000_0000_0000_0000_1000_0000_0000;
 
         /// The data of the file is not immediately available.
-        const FILE_ATTRIBUTE_OFFLINE                = 0b0000_0000_0000_1000_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_OFFLINE                = 0b0000_0000_0000_0000_0001_0000_0000_0000;
 
         /// The contents of the file need to be indexed.
-        const FILE_ATTRIBUTE_NOT_CONTENT_INDEXED    = 0b0000_0000_0000_0100_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_NOT_CONTENT_INDEXED    = 0b0000_0000_0000_0000_0010_0000_0000_0000;
 
         /// The file or directory is encrypted. For a file, this means that all data in the file is encrypted. For a directory, this means that encryption is the default for newly created files and subdirectories.
-        const FILE_ATTRIBUTE_ENCRYPTED              = 0b0000_0000_0000_0010_0000_0000_0000_0000;
+        const FILE_ATTRIBUTE_ENCRYPTED              = 0b0000_0000_0000_0000_0100_0000_0000_0000;
 
     }
 }
